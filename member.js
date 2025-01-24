a33b70c870412d2bc1f3e7f36d84ce7bc5f98cb9
harishmuh <149560161+harishmuh@users.noreply.github.com>
2025-01-24 04:45:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        skills: ['JavaScript', 'HTML', 'CSS'],
+        age: 34
+    };
+
+    // The member object is a reference to the object in the memory heap
+    return member;
+}
